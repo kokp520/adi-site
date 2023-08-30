a5f25fd7a6cf19b4350ac9f33848356eb24c7d40
adiwu <kokp520@gmail.com>
2023-08-30 08:56:14 +0800

    update new post

--- bin/cicd_script.go
+++ bin/cicd_script.go
@@ -8,56 +8,78 @@ import (
 )
 
 func main() {
-    // 設定 Hugo 專案目錄的路徑
-    hugoProjectPath := "/Users/adi.wu/blog/kokp520.github.io"
+	// 設定 Hugo 專案目錄的路徑
+	hugoProjectPath := "/Users/adi.wu/blog/kokp520.github.io"
 
-    hugoPath := "/Users/adi.wu/blog/adisite/"
+	hugoPath := "/Users/adi.wu/blog/adisite/"
 
-    // 執行 Hugo 命令
-    if err := executeCommand(hugoPath, "hugo"); err != nil {
-        fmt.Println("Hugo 執行失敗:", err)
-        return
-    }
+	// ----- website content -----
 
-    // 執行 Git 命令
-    if err := executeCommands(hugoProjectPath, "git", "add", "."); err != nil {
-        fmt.Println("Git add 失敗:", err)
-        return
-    }
+	// 執行 Git 命令
+	if err := executeCommands(hugoPath, "git", "add", "."); err != nil {
+		fmt.Println("Git add 失敗:", err)
+		return
+	}
 
-    if err := executeCommands(hugoProjectPath, "git", "commit", "-m", "update"); err != nil {
-        fmt.Println("Git commit 失敗:", err)
-        return
-    }
+	if err := executeCommands(hugoPath, "git", "commit", "-m", "update new post"); err != nil {
+		fmt.Println("Git commit 失敗:", err)
+		return
+	}
 
-    if err := executeCommands(hugoProjectPath, "git", "push"); err != nil {
-        fmt.Println("Git push 失敗:", err)
-        return
-    }
+	if err := executeCommands(hugoPath, "git", "push"); err != nil {
+		fmt.Println("Git push 失敗:", err)
+		return
+	}
 
-    fmt.Println("完成！")
+	fmt.Println("hugo path完成！")
+
+	// ---- public -----
+
+	// 執行 Hugo 命令
+	if err := executeCommand(hugoPath, "hugo"); err != nil {
+		fmt.Println("Hugo 執行失敗:", err)
+		return
+	}
+
+	// 執行 Git 命令
+	if err := executeCommands(hugoProjectPath, "git", "add", "."); err != nil {
+		fmt.Println("Git add 失敗:", err)
+		return
+	}
+
+	if err := executeCommands(hugoProjectPath, "git", "commit", "-m", "update"); err != nil {
+		fmt.Println("Git commit 失敗:", err)
+		return
+	}
+
+	if err := executeCommands(hugoProjectPath, "git", "push"); err != nil {
+		fmt.Println("Git push 失敗:", err)
+		return
+	}
+
+	fmt.Println("public完成！")
 }
 
 func executeCommand(dir, command string, args ...string) error {
-    cmd := exec.Command(command, args...)
-    cmd.Dir = dir
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	cmd := exec.Command(command, args...)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func executeCommands(dir, command string, args ...string) error {
-    cmd := exec.Command(command, args...)
-    cmd.Dir = dir
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    return cmd.Run()
+	cmd := exec.Command(command, args...)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func getAbsolutePath(relativePath string) (string, error) {
-    absPath, err := filepath.Abs(relativePath)
-    if err != nil {
-        return "", err
-    }
-    return absPath, nil
+	absPath, err := filepath.Abs(relativePath)
+	if err != nil {
+		return "", err
+	}
+	return absPath, nil
 }
